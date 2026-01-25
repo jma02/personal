b5341ef4181810a8c01bd96f8e2deaf29de29c6b
Jonathan Ma <ma.jonathan02@gmail.com>
2026-01-24 20:10:07 -0500

    try running the script

--- src/lib/notes/pdf-manifest.ts
+++ src/lib/notes/pdf-manifest.ts
@@ -1,4 +1,10 @@
 export const pdfNotes = [
+  "Analysis_I.pdf",
   "Finite_Geometry.pdf",
-  "Functional_Analysis.pdf"
+  "finite-element-method.pdf",
+  "Functional_Analysis.pdf",
+  "Linear_Algebra.pdf",
+  "measure-complex.pdf",
+  "Probability_I.pdf",
+  "stochastic-processes.pdf"
 ];
---
+++ static/notes/readme.txt
@@ -0,0 +1,1 @@
+Alot of course notes here aren't really refined or even completed. If you see any typos it would be nice to get an email about it: johnma@udel.edu
